d5e5217e40a70d8c1c55a7b40ba33222011cf0b9
Sergi Blanch-Torné <sergi@calcurco.cat>
2017-01-26 16:51:25 +0100

    work with the dealer component to show information about the running devices.

--- taurus-gui/widgets/dealer.py
+++ taurus-gui/widgets/dealer.py
@@ -25,8 +25,10 @@ __status__ = "development"
 import os
 from taurus import Attribute
 from taurus.core.taurusbasetypes import TaurusEventType
+from taurus.external.qt import QtGui
 from taurus.qt.qtgui.base import TaurusBaseComponent
 from taurus.qt.qtgui.container import TaurusWidget
+from taurus.qt.qtgui.display import TaurusLed
 from taurus.qt.qtgui.util.ui import UILoadable
 
 
@@ -87,8 +89,123 @@ class OptionsListener(TaurusBaseComponent):
             self.error("No widget set to introduce value names")
 
 
-# TODO: subscribe to runningDevices to populate the self.ui.tableLayout
-#       with device name, state, and the two extra attrs related with net.
+class RunningLayout(TaurusBaseComponent):
+    '''
+        subscribe to runningDevices to populate the self.ui.tableLayout
+        with device name, state, and the two extra attrs related with net.
+    '''
+    def __init__(self, parent=None, name=None, designMode=False):
+        # super(OptionsListener, self).__init__()
+        self.call__init__(TaurusBaseComponent, name, parent)
+
+    _widget = None
+    _rows = {}
+    _extraAttrs = ["ethernetframetransmissiondelay",
+                   "ethernetinterpacketdelay"]
+
+    def getWidget(self):
+        return self._widget
+
+    def setWidget(self, widget):
+        self._widget = widget
+
+    def setModel(self, model):
+        currentModel = self.getModel()
+        if model != currentModel:
+            if currentModel is None:
+                self._cleanLayout()
+            # FIXME: this is unnecessary in taurus4
+            TaurusBaseComponent.setModel(self, model)
+            try:  # taurus4
+                values = Attribute(model).rvalue
+            except:  # taurus3
+                values = Attribute(model).read().value
+            self._buildLayout(values)
+
+    def handleEvent(self, evt_src, evt_type, evt_value):
+        model = self.getModel()
+        if str(evt_src) != Attribute(model).getFullName():
+            self.warning("Event received from %s that doesn't "
+                         "correspond to the model %s" % (evt_src, model))
+            return
+        if evt_type in (TaurusEventType.Change,
+                        TaurusEventType.Periodic)\
+                and evt_value is not None:
+            self._buildLayout(evt_value.rvalue)
+        elif evt_type == TaurusEventType.Config:
+            self._buildLayout(evt_value.rvalue)
+        else:
+            self.warning("Unhandled type of event %s with value %s"
+                         % (evt_type, evt_value))
+
+    def _buildLayout(self, values):
+        if self._widget is not None:
+            new, existing, remove = self._separateValues(values)
+            for value in new:
+                if len(remove) != 0:
+                    self._replaceRow(value, self._rows[remove[0]])
+                    remove.pop(0)
+                else:
+                    self._newRow(value)
+            for value in remove:
+                self._removeRow(self._rows[value])
+                self._rows.pop(value)
+        else:
+            self.error("No widget set to introduce value names")
+
+    def _newRow(self, devName):
+        watchdogName = Attribute(self.getModel()).getParent().getNormalName()
+        row = self._widget.count()
+        self.info("New row %d -> %s" % (row, devName))
+        name = QtGui.QLabel("%s" % (devName))
+        self._widget.addWidget(name, row, 0)
+        state = TaurusLed()
+        state.setModel("%s/%s\\State" % (watchdogName,
+                                         devName.replace('/', '\\')))
+        self._widget.addWidget(state, row, 1)
+        rowDct = {}
+        rowDct['number'] = row
+        rowDct['name'] = name
+        rowDct['state'] = state
+        self._rows[devName] = rowDct
+
+    def _replaceRow(self, devName, row):
+        watchdogName = Attribute(self.getModel()).getParent().getNormalName()
+        self.info("Replace row %d: %s -> %s"
+                  % (row['number'], str(row['name'].text()), devName))
+        row['name'].setText("%s" % (devName))
+        ledModel = "%s/%s\\State" % (watchdogName,
+                                     devName.replace('/', '\\'))
+        print ledModel
+        row['state'].setModel(ledModel)
+
+    def _removeRow(self, row):
+        self.info("Remove row %d: %s"
+                  % (row['number'], str(row['name'].text())))
+        self._widget.removeWidget(row['name'])
+        self._widget.removeWidget(row['state'])
+        row['name'].deleteLater()
+        row['state'].deleteLater()
+
+    def _separateValues(self, values):
+        self.info("Preparing to process %s against %s"
+                  % (values, self._rows.keys()))
+        new, existing, remove = [], [], []
+        for value in values:
+            if value in self._rows:
+                existing.append(value)
+                self.info("\t%s exist" % (value))
+            else:
+                new.append(value)
+                self.info("\t%s new" % (value))
+        for value in self._rows:
+            if value not in values:
+                remove.append(value)
+                self.info("\t%s remove" % (value))
+        return new, existing, remove
+
+    def _cleanLayout(self):
+        pass
 
 
 ui_filename = "dealer.ui"
@@ -105,12 +222,13 @@ class Dealer(TaurusWidget):
         self.ui.options.setAutoApply(True)
         self._optionsListener = OptionsListener(self)
         self._optionsListener.setWidget(self.ui.options)
+        self._runningLayout = RunningLayout(self)
+        self._runningLayout.setWidget(self.ui.devList)
 
     _model = None
     _choose = "Dealer"
     _options = "Dealers"
-    _extraAttrs = ["ethernetframetransmissiondelay",
-                   "ethernetinterpacketdelay"]
+    _running = "RunningDevicesList"
 
     def getModel(self):
         return self._model
@@ -122,18 +240,21 @@ class Dealer(TaurusWidget):
                 self.debug("New model: %s" % (model))
                 choose = "%s/%s" % (model, self._choose)
                 options = "%s/%s" % (model, self._options)
-                self._setAttributes(choose, options)
+                running = "%s/%s" % (model, self._running)
+                self._setAttributes(choose, options, running)
                 self._model = model
             except Exception as e:
                 self.error("New model not valid: %s" % (e))
                 if self._model is not None:
                     choose = "%s/%s" % (self._model, self._choose)
                     options = "%s/%s" % (self._model, self._options)
-                    self._setAttributes(choose, options)
+                    running = "%s/%s" % (self._model, self._running)
+                    self._setAttributes(choose, options, running)
         else:
             self.warning("Set the same model than has")
 
-    def _setAttributes(self, choose, options):
+    def _setAttributes(self, choose, options, running):
         self.ui.choose.setModel(choose)
         self.ui.options.setModel(choose)
         self._optionsListener.setModel(options)
+        self._runningLayout.setModel(running)
